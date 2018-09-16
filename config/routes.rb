051eca23bcb264c7695befdc572b17267ed6669d
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-09-16 14:18:24 +0200

    setup i18n in application_controller.rb & config/application and nested routes with scope '(:locale)'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'albums#index'
-  resources :albums
+  scope '(:locale)', locale: /fr/ do
+    root to: 'albums#index'
+    resources :albums
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
