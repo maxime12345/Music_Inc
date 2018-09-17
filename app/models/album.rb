b25f4fae2ccd793d94a6e96690969ee1433c46e0
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-09-17 16:31:05 +0200

    add Album Factory

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,6 +3,6 @@
 class Album < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-  validates :title, :description, :author, :photo, presence: true
+  validates :title, :description, :author, presence: true
   belongs_to :user
 end
